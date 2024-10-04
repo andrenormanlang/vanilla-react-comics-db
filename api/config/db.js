17760717786d8582b3dd3f8da4abb696e15ef2ec
André Lang <72017716+andrenormanlang@users.noreply.github.com>
2024-10-04 09:38:08 +0200

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // PostgreSQL connection configuration
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:IN49KBWYHzXM@ep-weathered-darkness-a6roilpc.us-west-2.aws.neon.tech/dres_comics_db?sslmode=require'
+  connectionString: 'postgresql://neondb_owner:IN49KBWYHzXM@ep-aged-bird-a6no7ukt.us-west-2.aws.neon.tech/dres_comics_db?sslmode=require'
 });
 
 module.exports = pool;
